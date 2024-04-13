90fd74341f65e52c5ff40d4311ab26ab56fe234c
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-04-13 16:22:31 -0400

    wrote navbar

--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,11 @@ function App() {
   return (
     <div>
       <Navbar />
+      <Home />
+      <Home />
+      <Home />
+      <Home />
+      
     </div>
   );
 }
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,25 +1,32 @@
 import { Button, Navbar } from "flowbite-react";
+import navbarLogo from "../assets/navbarLogo.png";
+import "../App.css"
 
 function Component() {
   return (
-    <Navbar fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
-        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Button>Get started</Button>
+    <Navbar style={{ backgroundColor: '#000026' }}>
+      <div className="flex justify-between items-center w-full">
+        {/* Navbar Brand */}
+        <Navbar.Brand href="/">
+          <img src={navbarLogo} className="mr-3 h-20 sm:h-9" alt="Study Buddy Logo" />
+          <span className="self-center whitespace-nowrap text-2xl font-semibold text-white">Study Buddy</span>
+        </Navbar.Brand>
+
+        {/* Flex container for Links and Button */}
+        <div className="flex items-center pr-10">
+          {/* Navbar Links */}
+          <div className="flex list-none items-center px-10">
+            <Navbar.Link href="#" className="nav-link text-xl text-white px-4 py-2 relative">Home</Navbar.Link>
+            <Navbar.Link href="#" className="nav-link text-xl text-white px-4 py-2 relative">About Us</Navbar.Link>
+          </div>
+          
+          {/* Action Button */}
+          <Button>Study Now!</Button>
+        </div>
+
+        {/* Responsive Navbar Toggle */}
         <Navbar.Toggle />
       </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
-      </Navbar.Collapse>
     </Navbar>
   );
 }
