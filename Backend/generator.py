1f678b28544fb356b958a3cad9a2462c0916e1e4
prasadap <prasadap@umich.edu>
2024-04-13 13:32:57 -0400

    132pm

--- Backend/generator.py
+++ Backend/generator.py
@@ -1,46 +1,78 @@
 import google.generativeai as genai
-import pypdfium2 as pdfium
+import os
+import fitz  # pip install --upgrade pip; pip install --upgrade pymupdf
+from tqdm import tqdm # pip install tqdm
 from pypdf import PdfReader
+import glob
+import random
 
-genai.configure(api_key='AIzaSyA4U95OPm8gMXZ63Q63xjZCAydnb9mE0Tg')
+os.environ["GOOGLE_API_KEY"] = 'AIzaSyDK4aCLbBZxnDQtl6DWpoqEghI6yj49c1Y'
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
+workdir = "."
+os.mkdir(os.path.join(workdir, 'images'))
+
+for each_path in os.listdir(workdir):
+    if ".pdf" in each_path:
+        doc = fitz.Document((os.path.join(workdir, each_path)))
+        for i in tqdm(range(len(doc)), desc="pages"):
+            for img in tqdm(doc.get_page_images(i), desc="page_images"):
+                xref = img[0]
+                image = doc.extract_image(xref)
+                pix = fitz.Pixmap(doc, xref)
+                pix.save(os.path.join(os.path.join(workdir, 'images'), "%s_p%s-%s.png" % (each_path[:-4], i, xref)))
+                if os.stat(os.path.join(os.path.join(workdir, 'images'), "%s_p%s-%s.png" % (each_path[:-4], i, xref))).st_size < 10000:
+                    os.remove(os.path.join(os.path.join(workdir, 'images'), "%s_p%s-%s.png" % (each_path[:-4], i, xref)))
+
+input = []
 
 prompt = """I am having trouble understanding databases. Here are the lecture slides associated with it. 
 Can you summarize them for me in an easy to understand way? Then, can you make a multiple choice quiz for all of them, 
 with 5 questions from each slide deck, and give me the answer key at the end of each section?"""
 
+prompt2 = """I am having trouble understanding multicycle cpus. Here are some images from my lecture slides, can you 
+help me understand what's going on in 1 image?"""
+
 
 #  Return output in json format:
 # {Summary: summary}
 
-dict = {}
-list = []
-list.append(prompt)
+# reader = PdfReader("./pipelining.pdf")
+# text = ""
+# for page in reader.pages:
+#   text += page.extract_text() + "\n"
 
-#pdf = pdfium.PdfDocument("./Backend/CS121Lec01.pdf")
+# reader = PdfReader("2.pdf")
+# text2 = ""
+# for page in reader.pages:
+#   text2 += page.extract_text() + "\n"
 
-# image = genai.upload_file(path='./Backend/output_015.jpg')
-# response = model.generate_content([prompt, image])
-# print(response.text)
+# reader = PdfReader("3.pdf")
+# text3 = ""
+# for page in reader.pages:
+#   text3 += page.extract_text() + "\n"
+
+# input=[prompt, text]
+input2=[prompt2]
+size = len(os.listdir(os.path.join(workdir, "images")))
+listnum = random.sample(range(size), 5)
+counter = 0
+for img in os.listdir(os.path.join(workdir, "images")):
+        if counter in listnum:
+            print(str(img))
+            file = genai.upload_file(path=os.path.join(workdir, os.path.join("images", img)))
+            input2.append(file)
+            counter = counter + 1
 
 
-# for i in range(len(pdf)):
-#     page = pdf[i]
-#     image = page.render(scale=4).to_pil()
-#     image.save(f"./temp/output_{i:03d}.jpg")
-#     file = genai.upload_file(path=f'./temp/output_{i:03d}.jpg')
-#     list.append(file)
 
-# response = model.generate_content(list)
-# print(response.text)
-
-# print(".......")
-
-reader = PdfReader("./Backend/CS121Lec01.pdf")
-text = ""
-for page in reader.pages:
-  text += page.extract_text() + "\n"
-
-response = model.generate_content([prompt, text])
+import PIL.Image
+img = PIL.Image.open('./multicycle_cpu.png')
+response = model.generate_content([prompt2,img])
 print(response.text)
+
+files = glob.glob('./images/*')
+for f in files:
+    os.remove(f)
+os.rmdir('./images')
