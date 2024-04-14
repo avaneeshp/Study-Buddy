f302443cfeafde6af4fc2cfceee6ad943bf2f394
avaneeshp <avaneeshp@gmail.com>
2024-04-13 22:02:27 -0400

    Temporarily done with studdybuddy before merge

--- Backend/studybuddy.py
+++ Backend/studybuddy.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 import json
 import os
 from Backend.generator import *
+import random
+import glob
 
 app = Flask(__name__)
 CORS(app)
@@ -22,7 +24,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 results = []
-quizzes = []
 
 @app.route('/process_summary', methods=['POST'])
 def process_summary():
@@ -30,7 +31,6 @@ def process_summary():
     files = request.files.getlist('file')
     topic = request.form['topic']
     results.clear()
-    quizzes.clear()
     for file in files:
         if file and allowed_file(file.filename):
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
@@ -40,15 +40,24 @@ def process_summary():
             with open(outputpath, 'r') as out:
                 data = json.load(out)
                 results.append(data)
-            outputpath = './' + file.filename + '_quiz.json'
-            with open(outputpath, 'r') as out:
-                data = json.load(out)
-                quizzes.append(data)
     return jsonify(results)
 
 @app.route('/process_quiz', methods=['POST'])
 def process_quiz():
-    return jsonify(quizzes)
+    amt = request.form['amt']
+    quizzes = glob.glob('./*_quiz.json')
+    combined_questions = []
+    for file in quizzes:
+        with open(file.filename, 'r') as file:
+            quiz = json.load(file)
+            combined_questions.extend(quiz['quiz'].values())
+    random.shuffle(combined_questions)
+    if amt < len(combined_questions):
+        combined_questions = combined_questions[:amt]
+    new_quiz = {'quiz': {f'question{i+1}': q for i, q in enumerate(combined_questions)}}
+    # with open('./new_quiz.json', 'w') as file:
+    #     json.dump(new_quiz, file, indent=4) 
+    return jsonify(new_quiz)
     
 
 if __name__ == '__main__':
